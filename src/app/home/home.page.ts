bdbb2293eac9255a7c084252286281fd69da78ca
HARRY PAPADOPOULOS <harrispap03@gmail.com>
2021-11-16 15:46:25 +0200

    Pretter the user profile

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { OnInit } from '@angular/core';
 import { Router } from '@angular/router';
@@ -7,11 +7,11 @@ import { Router } from '@angular/router';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage implements AfterViewInit {
   currentUser: any;
   constructor(private auth: AuthService, private router: Router) {}
 
-  ngOnInit() {
+  ngAfterViewInit() {
     this.auth.user$.subscribe((user) => {
       if (user === null) {
         this.router.navigate(['user-profile']);
